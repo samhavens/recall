10a1ecf0a24f674e53b5ba6f6c4c27ff40f3bef0
Glitch (earthy-crack) <none>
2018-07-31 02:32:25 +0000

    🍰👔 Checkpoint ./skills/demo.js:12480/1530

--- skills/demo.js
+++ skills/demo.js
@@ -1,20 +1,21 @@
 /*
 
-Botkit Studio Skill module to enhance the "demo" script
+Botkit Studio Skill module to enhance the "recall" script
 
 */
 
+const url = (car) => `${process.env.NHTSA_API_ROOT}modelyear/${car.year}/make/${car.model}/model/${car.model}${process.env.NHTSA_API_SUFFIX}`
 
 module.exports = function(controller) {
     // define a before hook
     // you may define multiple before hooks. they will run in the order they are defined.
-    controller.studio.before('demo', function(convo, next) {
+    controller.studio.before('recall', function(convo, next) {
 
         // do some preparation before the conversation starts...
         // for example, set variables to be used in the message templates
         // convo.setVar('foo','bar');
 
-        console.log('BEFORE: demo');
+        console.log('BEFORE: recall', {...car});
       
         // don't forget to call next, or your conversation will never continue.
         next();
@@ -24,59 +25,14 @@ module.exports = function(controller) {
     /* Validators */
 
     // Validate user input: favorite_color
-    controller.studio.validate('demo','favorite_color', function(convo, next) {
+    controller.studio.validate('recall','car', function(convo, next) {
 
-        var value = convo.extractResponse('favorite_color');
+        var value = convo.extractResponse('car');
 
         // test or validate value somehow
         // can call convo.gotoThread() to change direction of conversation
 
         console.log('VALIDATE: demo VARIABLE: favorite_color');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_1
-    controller.studio.validate('demo','question_1', function(convo, next) {
-
-        var value = convo.extractResponse('question_1');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: demo VARIABLE: question_1');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_2
-    controller.studio.validate('demo','question_2', function(convo, next) {
-
-        var value = convo.extractResponse('question_2');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: demo VARIABLE: question_2');
-
-        // always call next!
-        next();
-
-    });
-
-    // Validate user input: question_3
-    controller.studio.validate('demo','question_3', function(convo, next) {
-
-        var value = convo.extractResponse('question_3');
-
-        // test or validate value somehow
-        // can call convo.gotoThread() to change direction of conversation
-
-        console.log('VALIDATE: demo VARIABLE: question_3');
 
         // always call next!
         next();
